fb67411e6c8d6d897d265b3f65e76de2179ba516
Tim Benniks <tim.benniks@valtech.com>
2019-10-10 12:14:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# stylelint-config-sonarqube
+
+A [Stylelint](https://stylelint.io/) config that attempts to map [sonar](https://www.sonarqube.org/) css rules to stylelint rules.
+
+## Mapping
+
+This [mapping](https://github.com/SymphonyOSF/sonar-css-plugin/blob/master/doc/stylelint-sonarqube-rule-mapping.md) has been used to come to the correct styleint rules to use.
+
+- "!important" annotation should not be used
+- CSS should be written in lower case
+- Duplicated properties should be removed
+- Each declaration should end with a semicolon
+- Empty declarations should be removed
+- Empty rules should be removed
+- Empty stylesheets should be removed
+- Experimental @-rules should not be used
+- Experimental identifiers should not be used
+- Experimental properties should not be used
+- Files should contain an empty new line at the end
+- Leading zeros should be removed
+- Lines should not be too long
+- Lines should not end with trailing whitespaces
+- Over-specified selectors should be simplified
+- Protocol-relative URL should not be used
+- Shorthand properties should be used whenever possible
+- Single quotes should be used instead of double quotes for strings
+- Source code should comply with formatting standards
+- There should be one single declaration per line
+- Trailing zeros for numeric values should be removed
+- Units for zero length values should be removed
+- Unknown @-rules should be removed
+- Unknown properties should be removed
+- Unknown pseudo-elements and pseudo-classes should be removed
+- Unknown type selectors should be removed
+- URL should be quoted
+
+## How to use
+
+Install the npm module:
+
+```
+yarn add stylelint-config-sonarqube
+npm install stylelint-config-sonarqube
+```
+
+Extend the Stylelint ruleset by adding this to your `.stylelintrc.js`:
+
+```javascript
+module.exports = {
+  extends: ["stylelint-config-valtech"]
+};
+```
+
+You can extend it with your own [rules](https://stylelint.io/user-guide/rules/) and [plugins](https://stylelint.io/user-guide/plugins/).
---
+++ index.js
@@ -0,0 +1,103 @@
+"use strict";
+
+// mapping used: https://github.com/SymphonyOSF/sonar-css-plugin/blob/master/doc/stylelint-sonarqube-rule-mapping.md
+
+module.exports = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    // "!important" annotation should not be used
+    "declaration-no-important": true,
+    "keyframe-declaration-no-important": true,
+
+    // CSS should be written in lower case
+    "property-case": "lower",
+    "function-name-case": "lower",
+    "at-rule-name-case": "lower",
+    "selector-pseudo-class-case": "lower",
+    "selector-pseudo-element-case": "lower",
+    "selector-type-case": "lower",
+    "unit-case": "lower",
+    "color-hex-case": "lower",
+
+    // Duplicated properties should be removed
+    "declaration-block-no-duplicate-properties": true,
+
+    // Each declaration should end with a semicolon
+    "declaration-block-trailing-semicolon": "always",
+
+    // Empty declarations should be removed
+    "no-extra-semicolons": true,
+
+    // Empty rules should be removed
+    "block-no-empty": true,
+
+    // Empty stylesheets should be removed
+    "no-empty-source": true,
+
+    // Experimental @-rules should not be used
+    "at-rule-no-vendor-prefix": true,
+
+    // Experimental identifiers should not be used
+    "value-no-vendor-prefix": true,
+
+    // Experimental properties should not be used
+    "property-no-vendor-prefix": true,
+
+    // Files should contain an empty new line at the end
+    "no-missing-end-of-source-newline": true,
+
+    // Leading zeros should be removed
+    "number-leading-zero": "never",
+
+    // Lines should not be too long
+    "max-line-length": 500,
+
+    // Lines should not end with trailing whitespaces
+    "no-eol-whitespace": true,
+
+    // Over-specified selectors should be simplified
+    "selector-max-compound-selectors": 8,
+
+    // Protocol-relative URL should not be used
+    "function-url-no-scheme-relative": true,
+
+    // Shorthand properties should be used whenever possible
+    "declaration-block-no-redundant-longhand-properties": true,
+
+    // Single quotes should be used instead of double quotes for strings
+    "string-quotes": "single",
+
+    // Source code should comply with formatting standards
+    "block-opening-brace-newline-after": "always",
+    "declaration-colon-space-after": "always",
+    "declaration-colon-space-before": "never",
+    "declaration-bang-space-after": "always",
+
+    // There should be one single declaration per line
+    "declaration-block-single-line-max-declarations": 1,
+
+    // Trailing zeros for numeric values should be removed
+    "number-no-trailing-zeros": true,
+
+    // Units for zero length values should be removed
+    "length-zero-no-unit": true,
+
+    "no-descending-specificity": false,
+
+    // Unknown @-rules should be removed
+    "at-rule-no-unknown": true,
+
+    // Unknown properties should be removed
+    "property-no-unknown": true,
+
+    // Unknown pseudo-elements and pseudo-classes should be removed
+    "selector-pseudo-class-no-unknown": true,
+    "selector-pseudo-element-no-unknown": true,
+
+    // Unknown type selectors should be removed
+    "selector-type-no-unknown": true,
+
+    // URL should be quoted
+    "function-url-quotes": "always"
+  }
+};
